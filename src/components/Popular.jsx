0513a9c810b3e49a3d129c8fecab81097803cd31
GR Pokhral <rahul.ajano@yahoo.com>
2022-05-10 16:18:49 +0545

    first commit

---
+++ src/components/Popular.jsx
@@ -0,0 +1,95 @@
+import { useState, useEffect } from "react";
+
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/css";
+
+const Popular = () => {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      "https://api.spoonacular.com/recipes/random?apiKey=d06ffb8505ec4833abb6a6eca068ff7c&number=09"
+    );
+    const data = await api.json();
+    setState(data.recipes);
+  };
+
+  return (
+    <>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "2rem",
+          }}
+        >
+          {state.map((val, id) => {
+            return (
+              <SplideSlide>
+                <Card key={id}>
+                  <p>{val.title}</p>
+                  <img src={val.image} alt={val.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 20rem;
+  overflow: hidden;
+  border-radius: 1.5rem;
+  position: relative;
+
+  img {
+    border-radius: 1.5rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
+
+export default Popular;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Popular from '../components/Popular'
+import Veg from '../components/Veg'
+
+const Home = () => {
+  return (
+    <>
+    <Veg/>
+    <Popular/>
+    </>
+  )
+}
+
+export default Home
